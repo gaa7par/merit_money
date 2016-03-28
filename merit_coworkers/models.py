e9c8768a7513eeced7ef1d38f786e4b9bf110623
gaa7par <gaa7par@gmail.com>
2016-03-29 01:11:40 +0200

    models added

--- merit_coworkers/models.py
+++ merit_coworkers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Coworker(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    avatar = models.ImageField(max_length=100)
+
+
+class Kudo(models.Model):
+    coworker = models.ForeignKey(Coworker, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    kudo = models.IntegerField(default=0)
+
