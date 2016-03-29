e88b3ee006b3ff153785e36e7ed3c12f078c1680
gaa7par <gaa7par@gmail.com>
2016-03-30 00:46:13 +0200

    __str__ method added to models

--- merit_coworkers/models.py
+++ merit_coworkers/models.py
@@ -8,9 +8,15 @@ class Coworker(models.Model):
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=30)
 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
 
 class Kudo(models.Model):
     coworker = models.ForeignKey(Coworker, on_delete=models.CASCADE)
     comment = models.CharField(max_length=200)
     kudo = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.comment
 
