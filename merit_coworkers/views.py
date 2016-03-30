2515f247e5bc6bd3e7017b586939e9a84f6a2ee3
gaa7par <gaa7par@gmail.com>
2016-03-30 22:46:02 +0200

    index view updated

--- merit_coworkers/views.py
+++ merit_coworkers/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Coworker
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Merit Coworkers")
+    coworkers = Coworker.objects.all()
+    first_name = [name.first_name for name in coworkers]
+    last_name = [name.last_name for name in coworkers]
+    output = first_name + [' '] + last_name
+    return HttpResponse(output)
 
 
 def info(request, coworker_id):
