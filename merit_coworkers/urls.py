f32872567ebe39ee73562fdf7cb6f930b46778f8
gaa7par <gaa7par@gmail.com>
2016-03-31 12:20:21 +0200

    form added

--- merit_coworkers/urls.py
+++ merit_coworkers/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'merit_coworkers'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<coworker_id>[0-9]+)/$', views.info, name='info'),
+    url(r'^(?P<coworker_id>[0-9]+)/info/$', views.info, name='info'),
+    url(r'^(?P<coworker_id>[0-9]+)/give_kudo/$', views.give_kudo, name='give_kudo'),
+    url(r'^(?P<coworker_id>[0-9]+)/results/$', views.results, name='results'),
 ]
 
--- merit_coworkers/views.py
+++ merit_coworkers/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from .models import Coworker
+from .models import Coworker, Kudo
 
 # Create your views here.
 
@@ -15,4 +17,27 @@ def info(request, coworker_id):
     coworker = get_object_or_404(Coworker, pk=coworker_id)
     context = {'coworker': coworker}
     return render(request, 'merit_coworkers/info.html', context)
+
+
+def give_kudo(request, coworker_id):
+    coworker = get_object_or_404(Coworker, pk=coworker_id)
+    context = {
+        'coworker': coworker,
+        'error_message': "You didn't give a kudo.",
+    }
+
+    try:
+        selected_kudo = coworker.kudo_set.get(pk=request.POST['kudo'])
+    except (KeyError, Kudo.DoesNotExist):
+        return render(request, 'merit_coworkers/info.html', context)
+    else:
+        selected_kudo.kudos += 1
+        selected_kudo.save()
+        return HttpResponseRedirect(reverse('merit_coworkers:results', args=(coworker.id,)))
+
+
+def results(request, coworker_id):
+    coworker = get_object_or_404(Coworker, pk=coworker_id)
+    context = {'coworker': coworker}
+    return render(request, 'merit_coworkers/results.html', context)
 
